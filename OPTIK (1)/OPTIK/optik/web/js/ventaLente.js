d7b7b8d4bb39734767215253e57dd20e3273101d
Gabriel Ramos <82692287+iamramosg@users.noreply.github.com>
2023-03-19 20:10:48 -0600

    ya cargan los select y guardan el objeto en una variable

--- OPTIK (1)/OPTIK/optik/web/js/ventaLente.js
+++ OPTIK (1)/OPTIK/optik/web/js/ventaLente.js
@@ -5,6 +5,10 @@ let armazones;
 let tratamientos;
 let materiales;
 let indexTV = 0;
+let examenSeleccionado;
+let micaSeleccionada;
+let materialSeleccionado;
+let armazonSeleccionado;
 
 // get All Cliente y cargar tabla cliente
 function getAllCliente() {
@@ -24,7 +28,6 @@ function getAllCliente() {
                     alert(data.error);
                 } else {
                     clientes = data;
-                    console.log(clientes);
                     cargarTablaClientes(data);
                 }
             });
@@ -51,36 +54,51 @@ function cargarTablaClientes(data) {
 
 //get All examen vista 
 function getAllExamenVista() {
-    const tbclientes = document.getElementById("tbCliente").getElementsByTagName("tr")[indexTV];
-    const idCliente = Number(tbclientes.getElementsByTagName("td")[0].textContent);
-    let datos = {id: idCliente};
-    let parametros = new URLSearchParams(datos);
+  const tbclientes = document.getElementById("tbCliente").getElementsByTagName("tr")[indexTV];
+  const idCliente = Number(tbclientes.getElementsByTagName("td")[0].textContent);
+  let datos = {id: idCliente};
+  let parametros = new URLSearchParams(datos);
 
-    fetch("http://localhost:8080/optik/api/restoptik/getAllExamenVista", {
-        method: "POST",
-        body: parametros,
-        headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
-    })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                if (data.error) {
-                    alert(data.error);
-                } else {
-                    examenesVista = data;
-                    console.log(examenesVista);
-                    // Obtener el select del HTML
-                    const selectExamen = document.getElementById("slcExamenVista");
+  fetch("http://localhost:8080/optik/api/restoptik/getAllExamenVista", {
+    method: "POST",
+    body: parametros,
+    headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    if (data.error) {
+      alert(data.error);
+    } else {
+      examenesVista = data;
+      // Obtener el select del HTML
+      const selectExamen = document.getElementById("slcExamenVista");
                     
-                    //Iterar sobre los objetos y agregar opciones al select 
-                    examenesVista.forEach(examen =>{
-                        const option = document.createElement("option");
-                        option.value = JSON.stringify(examen);
-                        option.text = examen.fecha;
-                        selectExamen.appendChild(option);
-                    });
-                }
-            });
+      // Crear opción predeterminada con el primer examen
+      const defaultOption = document.createElement("option");
+      defaultOption.text = "Seleccionar Examen";
+      if (examenesVista.length > 0) {
+        defaultOption.value = JSON.stringify(examenesVista[0]);
+        examenSeleccionado = examenesVista[0];
+      }
+      selectExamen.appendChild(defaultOption);
+                    
+      //Iterar sobre los objetos y agregar opciones al select 
+      examenesVista.forEach(examen =>{
+        const option = document.createElement("option");
+        option.value = JSON.stringify(examen);
+        option.text = examen.fecha;
+        selectExamen.appendChild(option);
+      });
+                    
+      //agregar un evento onchange al select 
+      selectExamen.addEventListener("change",() =>{
+        //obtener el valor seleccionado 
+        examenSeleccionado = JSON.parse(selectExamen.value);
+        console.log("el examen seleccionado es " + JSON.stringify(examenSeleccionado));
+      });
+    }
+  });
 }
 //get All tipo micas
 function getAllTipoMica() {
@@ -95,15 +113,30 @@ function getAllTipoMica() {
                     alert(data.error);
                 } else {
                     tipoMicas = data;
-                    console.log(tipoMicas);
-                    //pon que quieres que haga con la lista de tipo micas
+                    //obtener el select del HTML
                     const selectMica = document.getElementById("slcTipoMica");
                     
+                    //Crear opcion predeterminada con el primer tipo mica
+                    const defaultOption = document.createElement("option");
+                    defaultOption.text = "Seleccionar Mica";
+                    if(tipoMicas.length >0){
+                        defaultOption.value = JSON.stringify(tipoMicas[0]);
+                        micaSeleccionada = tipoMicas[0];
+                    }
+                    selectMica.appendChild(defaultOption);
+                    
+                    //iterar sobre los objetos y agregar las opciones al select
                     tipoMicas.forEach(mica =>{
                         const option = document.createElement("option");
                         option.value = JSON.stringify(mica);
                         option.text = mica.nombre;
                         selectMica.appendChild(option);
+                    });
+                    
+                    //agregar un evento onchange al select
+                    selectMica.addEventListener("change",() =>{
+                        //obtener el valor seleccionado
+                        micaSeleccionada = JSON.parse(selectMica.value);
                     });
                 }
             });
@@ -127,15 +160,29 @@ function getAllArmazon() {
                     alert(data.error);
                 } else {
                     armazones = data;
-                    console.log(armazones);
-                     
+                     //obtener el select del HTML
                      const selectArmazon = document.getElementById("slcArmazon");
+                     //Crear opción predeterminada en el primer armazon
+                     const defaultOption = document.createElement("option");
+                     defaultOption.text = "Seleccionar Armazon";
+                     if(armazones.length > 0){
+                         defaultOption.value = JSON.stringify(armazones[0]);
+                         armazonSeleccionado = armazones[0];
+                     }
+                     selectArmazon.appendChild(defaultOption);
                      
+                     //Iterar sobre los objetos y agregar opciones al select
                      armazones.forEach(armazon =>{
                          const option = document.createElement("option");
                          option.value = JSON.stringify(armazon);
                          option.text = armazon.producto.nombre;
                          selectArmazon.appendChild(option);
+                     });
+                     
+                     //agregar un evento onchange al select
+                     selectArmazon.addEventListener("change",() =>{
+                         //obtener el valor seleccionado
+                         armazonSeleccionado = JSON.parse(selectArmazon.value);
                      });
                 }
             });
@@ -159,7 +206,6 @@ function getAllTratamiento() {
                     alert(data.error);
                 } else {
                     tratamientos = data;
-                    console.log(tratamientos);
                     
                 }
             });
@@ -183,17 +229,44 @@ function getAllMateriales() {
                     alert(data.error);
                 } else {
                     materiales = data;
-                    console.log(materiales);
                     const selectMaterial = document.getElementById("slcMaterial");
+                    const defaultOption  = document.createElement("option");
+                    defaultOption.text = "Selecciona Material";
                     
+                    if(materiales.length > 0){
+                        defaultOption.value = JSON.stringify(materiales[0]);
+                        materialSeleccionado = materiales[0];
+                    }
+                    selectMaterial.appendChild(defaultOption);
                     materiales.forEach(material =>{
                         const option = document.createElement("option");
                         option.value = JSON.stringify(material);
                         option.text = material.nombre;
                         selectMaterial.appendChild(option);
                     });
+                    
+                    //agregar un evento onchange al select
+                    selectMaterial.addEventListener("change",() =>{
+                         //obtener el valor seleccionado
+                         materialSeleccionado = JSON.parse(selectMaterial.value);
+                     });
                 }
             });
 }
 
 
+function generarVentaLente() {
+    //creamos las claves 
+    const randomNum = Math.round(Math.random() * 100000000);
+    let clave = "OQ-" + randomNum;
+    let claveP = "OQ-" + randomNum;
+    
+    
+    //armamos el objeto de venta
+    var emp = JSON.parse(localStorage.getItem('currentUser'));
+    let venta = {clave: clave, empleado: emp};
+    
+     //armamos un objeto de presupuesto 
+     let presupuesto = {examenVista: examenSeleccionado, clave: claveP};
+     console.log(JSON.stringify(presupuesto));
+}
